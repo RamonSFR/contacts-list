1b22326c6e30e13ccec209f6a1b90aa652163295
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-09-13 17:48:53 -0300

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import GlobalStyle from './styles'
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  )
 }
 
 export default App
